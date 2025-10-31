68144925374f64345b6c5c543911619c164f1b63
Pranav-Padwale <pranavpadwale92@gmail.com>
2025-10-31 16:39:27 +0530

    generated secutity congig

--- src/main/kotlin/com/example/spring_crashcourse/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/spring_crashcourse/security/SecurityConfig.kt
@@ -1,4 +1,16 @@
 package com.example.spring_crashcourse.security
 
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
 class SecurityConfig {
+    fun filterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+        return httpSecurity
+            .csrf { csrf -> csrf.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .build()
+    }
 }
